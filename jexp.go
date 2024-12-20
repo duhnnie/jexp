86ce1239e10c66c99d0d303800b6dbe70896d9a1
Daniel Canedo <dnl.canedo@gmail.com>
2024-12-20 17:08:04 -0400

    WIP

--- jexp.go
+++ jexp.go
@@ -1,6 +1,8 @@
 package jexp
 
 import (
+	"reflect"
+
 	"github.com/duhnnie/jexp/expression"
 )
 
@@ -29,6 +31,32 @@ func New(variables VariableContainer) *JExp {
 	return &this
 }
 
+func resolve(j *JExp, v any) (interface{}, error) {
+	switch exp := v.(type) {
+	case uint, int, uint8, int8, uint16, int16, uint32, int32, uint64, int64, string, bool:
+		return v, nil
+	case *expression.OperationExpression:
+		return j.resolveOperationExpression(v.(*expression.OperationExpression))
+	case *expression.VariableExpression:
+		return j.resolveVariableExpression(v.(*expression.VariableExpression))
+	case *expression.ConstantExpression[int]:
+		return j.intConstResolver.Resolve(exp)
+	default:
+		return nil, ErrorCantResolveToExpression
+	}
+}
+
+func resolveToType[T any](j *JExp, e expression.Expression, out *T) error {
+	if res, err := resolve(j, e); err != nil {
+		return err
+	} else if v, ok := res.(T); ok {
+		*out = v
+		return nil
+	} else {
+		return ErrorCantResolveToType(reflect.ValueOf(out).Elem().Type().Name())
+	}
+}
+
 func (j *JExp) resolveOperationExpression(exp *expression.OperationExpression) (interface{}, error) {
 	switch exp.Type {
 	case expression.ExpTypeBooleanOperation:
@@ -54,49 +82,17 @@ func (j *JExp) resolveVariableExpression(exp *expression.VariableExpression) (in
 }
 
 func (j *JExp) Resolve(v any) (interface{}, error) {
-	switch exp := v.(type) {
-	case uint, int, uint8, int8, uint16, int16, uint32, int32, uint64, int64, string, bool:
-		return v, nil
-	case *expression.OperationExpression:
-		return j.resolveOperationExpression(v.(*expression.OperationExpression))
-	case *expression.VariableExpression:
-		return j.resolveVariableExpression(v.(*expression.VariableExpression))
-	case *expression.ConstantExpression[int]:
-		return j.intConstResolver.Resolve(exp)
-	default:
-		return nil, ErrorCantResolveToExpression
-	}
+	return resolve(j, v)
 }
 
 func (j *JExp) ResolveToBoolean(e expression.Expression, out *bool) error {
-	if res, err := j.Resolve(e); err != nil {
-		return err
-	} else if v, ok := res.(bool); ok {
-		*out = v
-		return nil
-	} else {
-		return ErrorCantResolveToType("bool")
-	}
+	return resolveToType(j, e, out)
 }
 
 func (j *JExp) ResolveToInt(e expression.Expression, out *int64) error {
-	if res, err := j.Resolve(e); err != nil {
-		return err
-	} else if v, ok := res.(int64); ok {
-		*out = v
-		return nil
-	} else {
-		return ErrorCantResolveToType("int64")
-	}
+	return resolveToType(j, e, out)
 }
 
 func (j *JExp) ResolveToUInt(e expression.Expression, out *uint64) error {
-	if res, err := j.Resolve(e); err != nil {
-		return err
-	} else if v, ok := res.(uint64); ok {
-		*out = v
-		return nil
-	}
-
-	return ErrorCantResolveToType("uint64")
+	return resolveToType(j, e, out)
 }
